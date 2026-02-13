fa28fe9b1d4b494ad141b400eb7d01018cb0e2d6
shivanshu-goswami <goswamishivanshu47@gmail.com>
2026-02-13 15:03:58 +0530

    Syntax and Variable

---
+++ SyntaxAndVariable.js
@@ -0,0 +1,22 @@
+let a = 1; console.log(a);
+const b = 2; console.log(b);
+var c = 3; console.log(c);
+
+let name = "Alice"; console.log(name);
+const age = 25; console.log(age);
+
+let total = 5 + 3; console.log(total);
+let diff = 10 - 4; console.log(diff);
+
+let mult = 3 * 7; console.log(mult);
+let div = 20 / 5; console.log(div);
+
+let mod = 10 % 3; console.log(mod);
+let pow = 2 ** 3; console.log(pow);
+
+let str1 = "Hello"; let str2 = "World"; 
+console.log(str1 + " " + str2);
+let isTrue = true; console.log(isTrue);
+
+let x = 5; x += 3; console.log(x);
+let y = 8; y *= 2; console.log(y);
